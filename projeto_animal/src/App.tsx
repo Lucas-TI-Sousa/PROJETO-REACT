ae4d42fa91ac80ab8918033feee1475ab0730c6a
lucas <lucas.ssantos189@senacsp.edu.br>
2024-11-25 21:51:32 -0300

    testes

--- projeto_animal/src/App.tsx
+++ projeto_animal/src/App.tsx
@@ -1,9 +1,5 @@
-import { Redirect, Route } from 'react-router-dom';
-import { IonApp, IonRouterOutlet, setupIonicReact } from '@ionic/react';
-import { IonReactRouter } from '@ionic/react-router';
-import Home from './pages/Home';
-
-/* Core CSS required for Ionic components to work properly */
+import React from 'react';
+import { IonNav } from '@ionic/react';
 import '@ionic/react/css/core.css';
 
 /* Basic CSS for apps built with Ionic */
@@ -30,27 +26,11 @@ import '@ionic/react/css/display.css';
 /* import '@ionic/react/css/palettes/dark.class.css'; */
 import '@ionic/react/css/palettes/dark.system.css';
 
-/* Theme variables */
-import './theme/variables.css';
+import PageOne from './page-one';
 
-setupIonicReact();
-
-const App: React.FC = () => (
-  <IonApp>
-    <IonReactRouter>
-      <IonRouterOutlet>
-
-        <Route exact path="/home">
-          <Home />
-        </Route>
-
-        <Route exact path="/">
-          <Redirect to="/home" />
-        </Route>
-        
-      </IonRouterOutlet>
-    </IonReactRouter>
-  </IonApp>
-);
-
-export default App;
+function Example() {
+  return (<>ok
+  <IonNav root={() => <PageOne />}></IonNav>
+  </>);
+}
+export default Example;
